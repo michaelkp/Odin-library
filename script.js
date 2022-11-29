58c1943b63acbabacb907ce9e6e115c8383f3a53
Michael <michaelk99@gmail.com>
2022-11-29 04:23:39 -0800

    Initial commit and start Library project.

---
+++ script.js
@@ -0,0 +1,23 @@
+'use strict'
+
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.info = function() {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}.`
+    }
+
+    addBookToLibrary(this)
+}
+
+const theHobbit = new Book('Hobbit', 'Tolkien', 250, 'not read yet')
+console.log(theHobbit.info())
+
+function addBookToLibrary() {
+    myLibrary.push(this.info)
+    console.log(myLibrary + ' --push myLibrary ')
+}
