d93e94f804ac70ce9cde9ca34b02ec989b44912c
Michael <michaelk99@gmail.com>
2022-11-29 05:48:26 -0800

    Add code to open dialog box and cancel button to close box.

--- script.js
+++ script.js
@@ -2,22 +2,47 @@
 
 let myLibrary = [];
 
+const newBookBtn = document.getElementById('newBookBtn')
+newBookBtn.addEventListener('pointerup', () => {
+    console.log('button test')
+    dialog.showModal()
+    openCheck(dialog)
+})
+const dialog = document.getElementById('newBookDialog')
+const cancelBtn = document.getElementById('cancelBtn')
+cancelBtn.addEventListener('pointerup', () => {
+    console.log('Cancel pressed')
+    dialog.close('Cancel')
+    openCheck(dialog)
+})
+const saveBtn = document.getElementById('saveBtn')
+
+function openCheck(dialog) {
+    if(dialog.open) {
+        console.log('Dialog open!')
+    } else {
+        console.log('Dialog closed.')
+    }
+}
+
+
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
-    this.info = function() {
+    this.info = () => {
         return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}.`
     }
-
     addBookToLibrary(this)
 }
 
-const theHobbit = new Book('Hobbit', 'Tolkien', 250, 'not read yet')
-console.log(theHobbit.info())
+// const theHobbit = new Book('Hobbit', 'Tolkien', 250, 'not read yet')
+// console.log(theHobbit.info())
 
 function addBookToLibrary() {
-    myLibrary.push(this.info)
+
+
+    myLibrary.push()
     console.log(myLibrary + ' --push myLibrary ')
 }
