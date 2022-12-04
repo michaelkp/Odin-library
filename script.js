9d2f7da071a5b9283aeeec244bc31e8a26b10d18
Michael <michaelk99@gmail.com>
2022-12-03 17:02:28 -0800

    Add cardId to constructor Book.

--- script.js
+++ script.js
@@ -1,28 +1,6 @@
 'use strict'
 
-let myLibrary = [
-    // {
-    //     bookId: '001',
-    //     title: 'hobbit',
-    //     author: 'tolkien',
-    //     pages: 231,
-    //     hasRead: 'read'
-    // },
-    // {   
-    //     bookId: '002',
-    //     title: 'frogger',
-    //     author: 'sega',
-    //     pages: 23,
-    //     hasRead: 'read'
-    // },
-    // {
-    //     bookId: '003',
-    //     title: 'sandman',
-    //     author: 'gaiman',
-    //     pages: 1003,
-    //     notRead: 'read'
-    // }
-];
+let myLibrary = []
 
 // button to open dialog box with form
 const newBookBtn = document.getElementById('newBookBtn')
@@ -56,20 +34,18 @@ function openCheck(dialog) {
         console.log('Dialog closed.')
     }
 }
-
+let cardId = 1
 function userInput(returnValue) {
     if(!returnValue || returnValue === 'Cancel') {
         return
     } else if(returnValue === 'Save') {
-        
+        // let bookId = 0
         const newBook = new Book(
-                0,
+                cardId++,
                 title.value,
                 author.value,
                 pages.value,
                 hasRead.value,
-                
-
             )
             // newBook.description = description.value
             console.log(newBook instanceof Book);
@@ -78,112 +54,83 @@ function userInput(returnValue) {
 }
 // end of dialog box
 
-function Book(bookId, title, author, pages, read, description) {
-    this.bookId = () => {
-        return bookId++
-    }
-    console.log(this.bookId() + ' -- BOOKID')
+
+
+function Book(cardId,title, author, pages, read) {
+    this.cardId = cardId
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
     // this.description = description
     this.info = () => {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}.`
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}. ID = ${cardId}`
     }
 }
 
+// Book.prototype.cardId = function() {
+//     this.cardId = 0
+//     console.log('cardid test');
+// }
+
 // sample books
-const theHobbit = new Book(1, 'The Hobbit', 'Tolkien', 250,)
-const theSandman = new Book(2, 'The Sandman', 'Niel Gaiman', 1200)
-const hamlet = new Book(3, 'William Shakespeare', 150)
+const theHobbit = new Book(1,'The Hobbit', 'J.R.R. Tolkien', 250,)
+const theSandman = new Book(2,'The Sandman', 'Niel Gaiman', 1200)
+const hamlet = new Book(3, 'Hamlet', 'William Shakespeare', 150)
 
 addBookToLibrary(theHobbit)
 addBookToLibrary(theSandman)
 addBookToLibrary(hamlet)
+// end samples
+
 
 function addBookToLibrary(newBook) {
-    console.log(newBook.title + ' --title in addBook')
-console.log(newBook.info());
+    console.log(newBook.info());
+    // console.log(newBook.id + " --ID");
     myLibrary.push(newBook)
-    console.log(myLibrary + ' --push myLibrary ')
+    // let id = newBook[id]
+    // for(newBook in myLibrary) {
+    //     newBook[id++]
+    //     console.log(id + ' --ID test');
+    // }
     makeCard(newBook)
 }
 
-
+// const incrementCardIdBy1 = (card) => {
+//     cardId++
+// }
 function makeCard(myLibrary) {
     // display new book 
     const newestBook = myLibrary
-    console.log(newestBook)
     const cardContainer = document.querySelector('.card-container')
     const card = document.createElement('div')
         card.className = 'card'
-        card.id = 0
-
-        // let cardArray = []
-        // cardArray.push(card)
-        // let cardId = card.dataset.cardId
-        // cardArray.forEach(cardId => {
-        //     cardId++
-        //     console.log(cardArray + ' --card array');
-        //     console.log(cardId + ' -- id test');
-        //     console.log(card.dataset.cardId );
-        // })
+        // card.id = 0
+        // let cardId = card.id
+        // incrementCardIdBy1(cardId)
     const cardHeader = document.createElement('header')
     const cardList = document.createElement('ul')
     const cardLiAuthor = document.createElement('li')
     const cardLiPages = document.createElement('li')
-    console.log(card.id + ' -- cardId')
 
         card.appendChild(cardHeader)
             cardHeader.textContent = newestBook.title
             console.log(newestBook.title)
+            console.log(newestBook.cardId + ' --ID in makecard');
         card.appendChild(cardList)
         cardList.appendChild(cardLiAuthor)
             cardLiAuthor.textContent = newestBook.author
         cardList.appendChild(cardLiPages)
             cardLiPages.textContent = newestBook.pages
         cardContainer.appendChild(card)
-        // const addCardIdCounter = (card) => {
-        //     const cardCounter = document.getElementsByClassName('card')
-        //     console.log(cardCounter + ' -- card counter');
-        //     // let cardArray = []
-        //         // cardArray.push(cardCounter)
-        //         // console.log(cardArray[0] + ' -- card array');
-        //         let cardId = card.id
-        //         console.log(cardId + ' --id');
-        //         for(cardId in cardCounter) {
-        //             cardId++
-        //             console.log(cardId + ' --card id');
-        //         }
-        //         // cardCounter.forEach(cardId => {
-        //         //     cardId + 1
-        //         //     // console.log(cardArray + ' --card array');
-        //         //     console.log(cardId + ' -- id test');
-        //         //     // console.log(card.dataset.cardId );
-        //         // })
-        //         return cardId
-        // }  
-        // addCardIdCounter(card)      
+        // console.log(cardId + ' --CARDID test'); 
     return card
 }
 
-// function addCardIdCounter(card) {
-//     const cardCounter = document.getElementsByClassName('card')
-//     console.log(cardCounter + ' -- card counter');
-//     // let cardArray = []
-//         // cardArray.push(cardCounter)
-//         // console.log(cardArray[0] + ' -- card array');
-//         let cardId = card.id
-//         console.log(cardId + ' --id');
-//         for(cardId in cardCounter) {
-//             cardId++
-//             console.log(cardId + ' --card id');
-//         }
-//         // cardCounter.forEach(cardId => {
-//         //     cardId + 1
-//         //     // console.log(cardArray + ' --card array');
-//         //     console.log(cardId + ' -- id test');
-//         //     // console.log(card.dataset.cardId );
-//         // })
+// function incrementCardIdBy1(cardId) {
+    
+//     cardId++
+//     console.log(cardId + ' --CardId TEST');
 // }
+
+
